091635fc4badc0673b42b98e42a1f29cf958aa08
B4si1 <153111672+B4si1@users.noreply.github.com>
2024-03-31 02:46:19 +0100

    trader script

--- BasilLand/scripts/trader-script.js
+++ BasilLand/scripts/trader-script.js
@@ -1,174 +1,172 @@
 import { worldLogic } from "./game-loop-script.js";
 import { LogDisplay } from "./game-loop-script.js";
 
-import { refineIdeasBtn, towerDisplay } from "./doc-object-script.js";
-
 import { hide } from "./helper-functions.js";
 
-import { towerFoodBuffBtn } from "./doc-object-script.js";
-import { towerWoodBuffBtn } from "./doc-object-script.js";
-import { towerStoneBuffBtn } from "./doc-object-script.js";
-import { towerIdeaBuffBtn } from "./doc-object-script.js";
-import { towerGoldBuffBtn } from "./doc-object-script.js";
-import { towerHouseBuffBtn } from "./doc-object-script.js";
-import { towerWorkerBuffBtn } from "./doc-object-script.js";
-import { towerExitBtn } from "./doc-object-script.js";
+import { traderExitBtn } from "./doc-object-script.js";
+import { traderDisplay } from "./doc-object-script.js";
+import { openTraderBtn } from "./doc-object-script.js";
 
-import { openTowerBtn } from "./doc-object-script.js";
-import { buffer } from "./buff-display-script.js";
+const sellFoodBtn = document.getElementById('sell-food')
+const sellWoodBtn = document.getElementById('sell-wood')
+const sellStoneBtn = document.getElementById('sell-stone')
+const sellWorkerBtn = document.getElementById('sell-worker')
 
-let buffCost = 50
-let goldBuffCost = 200
-let houseSpawnBuff = 500
-let refineCost = 200
-let refinedIdeasGain = 5
+const buyFoodBtn = document.getElementById('buy-food')
+const buyWoodBtn = document.getElementById('buy-wood')
+const buyStoneBtn = document.getElementById('buy-stone')
+const buyWorkerBtn = document.getElementById('buy-worker')
 
-export function updateTowerBtnDisplay(){
-    if(worldLogic.idea <= refineCost){
-        refineIdeasBtn.classList.add('unable')
+const minFoodTrade = 101
+const minWoodTrade = 101
+const minStoneTrade = 50
+const minWorkerTrade = 1
+const minGoldTrade = 1
+
+
+export function updateTraderButtonDisplay(){
+    traderSellButtonHelper(worldLogic.food, minFoodTrade, sellFoodBtn)
+    traderSellButtonHelper(worldLogic.wood, minWoodTrade, sellWoodBtn)
+    traderSellButtonHelper(worldLogic.stone, minStoneTrade, sellStoneBtn)
+    traderSellButtonHelper(worldLogic.idleWorkers, minWorkerTrade, sellWorkerBtn)
+
+    traderBuyButtonHelper(worldLogic.gold, minGoldTrade, buyFoodBtn)
+    traderBuyButtonHelper(worldLogic.gold, minGoldTrade, buyWoodBtn)
+    traderBuyButtonHelper(worldLogic.gold, minGoldTrade, buyStoneBtn)
+    traderBuyButtonHelper(worldLogic.gold, 100, buyWorkerBtn)
+
+
+}
+
+function traderSellButtonHelper(resource, minresource, button){
+    if(resource < minresource){
+        button.classList.add('unable')
     }else{
-        refineIdeasBtn.classList.remove('unable')
+        button.classList.remove('unable')
     }
 }
 
-refineIdeasBtn.addEventListener('click', function(e){
-    if(worldLogic.idea <= refineCost){
-        LogDisplay.eventLog('Not Enough ideas!', 'red', 'alert')
+function traderBuyButtonHelper(gold, minGold, button){
+    if(gold < minGold){
+        button.classList.add('unable')
     }else{
-        worldLogic.idea -= refineCost;
-        worldLogic.refinedIdeas += refinedIdeasGain;
-        LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/refinedidea.png"></img> Refined 200 Ideas ', 'yellow') // <img class="log-img-scale" src="./BasilLand/images/refinedidea.png"></img>
+        button.classList.remove('unable')
+    }
+}
+
+sellFoodBtn.addEventListener('click', function(e){
+    if(worldLogic.food > minFoodTrade){
+        worldLogic.food = worldLogic.food - 100;
+        worldLogic.gold++;
+        LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Sold [100] food for [1] gold!', 'yellow')
+    } else {
+        LogDisplay.eventLog('Not enough food!', 'red', 'alert')
     }
 })
 
-towerFoodBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.refinedIdeas >= buffCost){
-        if(worldLogic.towerBuffProductionStatus.food == false){
-            worldLogic.towerBuffProductionStatus.food = true;
-            if(buffer.buffs.food.status == false){
-                buffer.buffs.food.status = true
-            }
-            worldLogic.refinedIdeas -= buffCost
-            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/farming.png"></img> Farming buff [10] Days', 'purple')
-        }else{
-            LogDisplay.eventLog('Buff already active!' , 'red', 'alert')
-        }
-    }else{
-        LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
+sellWoodBtn.addEventListener('click', function(e){
+    if(worldLogic.wood > minWoodTrade){
+        worldLogic.wood = worldLogic.wood - 100;
+        worldLogic.gold++;
+        LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Sold [100] wood for [1] gold!', 'yellow')
+    } else {
+        LogDisplay.eventLog('Not enough wood!', 'red', 'alert')
     }
 })
 
-towerWoodBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.refinedIdeas >= buffCost){
-        if(worldLogic.towerBuffProductionStatus.wood == false){
-            worldLogic.towerBuffProductionStatus.wood = true;
-            if(buffer.buffs.wood.status == false){
-                buffer.buffs.wood.status = true
-            }
-            worldLogic.refinedIdeas -= buffCost
-            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/lumbering.png"></img> Lumber buff [10] Days', 'purple')
-        }else{
-            LogDisplay.eventLog('Buff already active!' , 'red', 'alert')
-        }
-    }else{
-        LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
+sellStoneBtn.addEventListener('click', function(e){
+    if(worldLogic.stone > minStoneTrade){
+        worldLogic.stone = worldLogic.stone - 50;
+        worldLogic.gold++;
+        LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Sold [100] stone for [1] gold!', 'yellow')
+    } else {
+        LogDisplay.eventLog('Not enough stone!', 'red', 'alert')
     }
 })
 
-towerStoneBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.refinedIdeas >= buffCost){
-        if(worldLogic.towerBuffProductionStatus.stone == false){
-            worldLogic.towerBuffProductionStatus.stone = true;
-            if(buffer.buffs.stone.status == false){
-                buffer.buffs.stone.status = true
-            }
-            worldLogic.refinedIdeas -= buffCost
-            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/mining.png"></img> Mining buff [10] Days', 'purple')
-        }else{
-            LogDisplay.eventLog('Buff already active!' , 'red', 'alert')
+sellWorkerBtn.addEventListener('click', function(e){
+    if(worldLogic.idleWorkers > 0){
+        if(worldLogic.workers > minWorkerTrade){
+            worldLogic.workers = worldLogic.workers - 1;
+            worldLogic.gold += 10;
+            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Sold [1] worker for [10] gold!', 'yellow')
+        } else {
+            LogDisplay.eventLog('Not enough workers!', 'red', 'alert')
         }
     }else{
-        LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
-    }
-})
-
-towerIdeaBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.refinedIdeas >= buffCost){
-        if(worldLogic.towerBuffProductionStatus.idea == false){
-            worldLogic.towerBuffProductionStatus.idea = true;
-            if(buffer.buffs.idea.status == false){
-                buffer.buffs.idea.status = true
-            }
-            worldLogic.refinedIdeas -= buffCost
-            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/thinking.png"></img> Thinking buff [10] Days', 'purple')
-        }else{
-            LogDisplay.eventLog('Buff already active!' , 'red', 'alert')
-        }
-    }else{
-        LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
-    }
-})
-
-towerGoldBuffBtn.addEventListener('click', function(e){
-    if (worldLogic.goldFind.level < 50){
-        if(worldLogic.refinedIdeas >= goldBuffCost){
-            worldLogic.goldFind.level ++;
-            worldLogic.goldFind.chance ++;
-            worldLogic.refinedIdeas -= goldBuffCost
-            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/goldfind.png"></img> Gold Find Chance Increased', 'yellow')
-        }else{
-            LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
-        }
-    }else{
-        LogDisplay.eventLog(`Gold Find Maxed`, 'red', 'alert')
+        LogDisplay.eventLog('Can only sell Idle Workers!', `red`, 'alert')
     }
     
 })
 
-towerHouseBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.towerUpgradeBuff.houseBuffStatus == false){
-        if(worldLogic.refinedIdeas > houseSpawnBuff){
-            worldLogic.refinedIdeas -= houseSpawnBuff
-            worldLogic.houseCapacity *= worldLogic.towerUpgradeBuff.housecapacity
-            worldLogic.maxWorkers *= worldLogic.towerUpgradeBuff.housecapacity
-            LogDisplay.eventLog(`<img class="log-img-scale" src="./BasilLand/images/roomupgrade.png"></img> House Buff Applied`, 'purple')
-            worldLogic.towerUpgradeBuff.houseBuffStatus = true
-            buffer.buffs.house.status = true
-        }else{
-            LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
+buyFoodBtn.addEventListener('click', function(e){
+    if(worldLogic.food < (worldLogic.townStorage.food * worldLogic.storage) - 99){
+        if(worldLogic.gold >= minGoldTrade){
+            worldLogic.food = worldLogic.food + 100;
+            worldLogic.gold--;
+            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Bought [100] food for [1] gold!', 'yellow')
+        } else {
+            LogDisplay.eventLog('Not enough gold!', 'red', 'alert')
         }
     }else{
-        LogDisplay.eventLog(`House Buff Already Applied`, 'red', 'alert')
+        LogDisplay.eventLog('Not enough storage!', 'red', 'alert')
     }
+    
 })
 
-
-towerWorkerBuffBtn.addEventListener('click', function(e){
-    if(worldLogic.towerUpgradeBuff.workerBuffStatus == false){
-        if(worldLogic.refinedIdeas > houseSpawnBuff){
-            worldLogic.refinedIdeas -= houseSpawnBuff
-            worldLogic.workerSpawnRate = 2;
-            LogDisplay.eventLog(`<img class="log-img-scale" src="./BasilLand/images/twinupgrade.png"></img> Twins Buff Applied`, 'purple')
-            worldLogic.towerUpgradeBuff.workerBuffStatus = true
-            buffer.buffs.worker.status = true
-        }else{
-            LogDisplay.eventLog('Not enough refined ideas', 'red', 'alert')
+buyWoodBtn.addEventListener('click', function(e){
+    if(worldLogic.wood < (worldLogic.townStorage.wood * worldLogic.storage) - 99){
+        if(worldLogic.gold >= minGoldTrade){
+            worldLogic.wood = worldLogic.wood + 100;
+            worldLogic.gold--;
+            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Bought [100] wood for [1] gold!', 'yellow')
+        } else {
+            LogDisplay.eventLog('Not enough gold!', 'red', 'alert')
         }
     }else{
-        LogDisplay.eventLog(`House Buff Already Applied`, 'red', 'alert')
+        LogDisplay.eventLog('Not enough storage!', 'red', 'alert')
     }
+    
 })
 
+buyStoneBtn.addEventListener('click', function(e){
+    if(worldLogic.stone < (worldLogic.townStorage.stone * worldLogic.storage) - 49){
+        if(worldLogic.gold >= minGoldTrade){
+            worldLogic.stone = worldLogic.stone + 50;
+            worldLogic.gold--;
+            LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Bought [50] stone for [1] gold!', 'yellow')
+        } else {
+            LogDisplay.eventLog('Not enough gold!', 'red', 'alert')
+        }
+    }else{
+        LogDisplay.eventLog('Not enough storage!', 'red', 'alert')
+    }
+    
+})
 
-towerExitBtn.addEventListener('mouseover', function(e){
+buyWorkerBtn.addEventListener('click', function(e){
+        if(worldLogic.gold > 100){
+        
+            if(worldLogic.workers < worldLogic.maxWorkers){
+                worldLogic.workers++;
+                worldLogic.gold - worldLogic.gold - 100;
+                LogDisplay.eventLog('<img class="log-img-scale" src="./BasilLand/images/trader-seller.png"></img> Bought [1] worker for [100] gold!', 'yellow')
+            }else{LogDisplay.eventLog('Town at capacity!', 'red', 'alert')}
+        
+        } else {
+            LogDisplay.eventLog('Not enough gold!', 'red', 'alert')
+        }    
+})
+
+traderExitBtn.addEventListener('mouseover', function(e){
     e.target.src = './BasilLand/images/open.png'
 })
 
-towerExitBtn.addEventListener('mouseleave', function(e){
+traderExitBtn.addEventListener('mouseleave', function(e){
     e.target.src = './BasilLand/images/closed.png'
 })
 
-towerExitBtn.addEventListener('click', function(e){
-    hide(towerDisplay)
-    openTowerBtn.innerHTML = `Enter Tower`
+traderExitBtn.addEventListener('click', function(e){
+    hide(traderDisplay)
+    openTraderBtn.innerHTML = `Trade`
 })
