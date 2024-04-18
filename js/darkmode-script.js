4ca631f726d9687d95fb5eed31e07ab963cb280f
B4si1 <153111672+B4si1@users.noreply.github.com>
2024-04-18 03:17:11 +0100

    emojis

--- js/darkmode-script.js
+++ js/darkmode-script.js
@@ -7,9 +7,9 @@ darkMode.addEventListener('mouseup', function(e){
 function checkDarkModeState(){
     if(localStorage.getItem('NightMode') == 'light'){
         document.body.classList.add('light')
-        darkMode.value = 'Light mode active';
+        darkMode.innerHTML = "Dark 🌑 ";
     } else {
-        darkMode.value = 'Dark mode active';
+        darkMode.innerHTML = "Light 🌕";
     }
 }
 
@@ -33,11 +33,11 @@ function light(x){
     if(x == 'add'){
         document.body.classList.add('light')
         localStorage.setItem('NightMode', 'light');
-        darkMode.innerHTML = "Dark";
+        darkMode.innerHTML = "Dark 🌑 ";
     }else{
         document.body.classList.remove('light')
         localStorage.setItem('NightMode', 'dark');
-        darkMode.innerHTML = "Light";
+        darkMode.innerHTML = "Light 🌕";
     }
     
 }
